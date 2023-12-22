bd88a4c470e1324e901ee9c51fc0b6522d12a64c
djiby thioub <thioubdjiby96@gmail.com>
2023-12-22 10:33:36 +0000

    add getstream

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StreamChat, User } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'stream-chat-react/dist/css/v2/index.css';
+
+const userId = 'spring-mountain-4';
+const userName = 'spring-mountain-4';
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?id=${userId}&name=${userName}`,
+};
+
+const apiKey = 'h5aw3vc3kqfy';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic3ByaW5nLW1vdW50YWluLTQifQ.of6ocgdNp5rc4ld5utHyzebYE7rBlZSOqUyZXFmjiJ0';
+
+const chatClient = new StreamChat(apiKey);
+chatClient.connectUser(user, userToken);
+
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Tooshare',
+  members: [userId],
+});
+
+const App = () => (
+  <Chat client={chatClient} theme='str-chat__theme-light'>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
